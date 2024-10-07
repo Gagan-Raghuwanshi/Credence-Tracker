9eb9867410b0cb36d4e97fa0533a0788c78252b1
Yash Dahiwale <yashdahiwale3@gmail.com>
2024-10-07 18:10:35 +0530

    fixed alerts conflict

--- utils/alert.utils.js
+++ utils/alert.utils.js
@@ -1,75 +1,90 @@
-import express from 'express'; 
-import http from 'http'; 
-import { Server } from 'socket.io'; 
-import axios from 'axios'; 
-import moment from 'moment'; 
-import { Notification } from '../models/notification.model.js'; 
-import Alert from '../models/alert.model.js'; 
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import axios from 'axios';
+import moment from 'moment';
+import { Notification } from '../models/notification.model.js';
+import Alert from '../models/alert.model.js';
 
-const app = express(); 
+const app = express();
 let deviceStatus = {};
-const stopLimit = 1; 
-let deviceSpeed; 
-let data = null; 
+const stopLimit = 1;
+let data = null;
 let alertsArray = [];
+const inactiveThreshold = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+// const inactiveThreshold = 60 * 1000; // 1 minute
 
-
-const checkDeviceStatus = (io,deviceData) => {
+const checkDeviceStatus = (io, deviceData) => {
     const { deviceId, status, attributes: { ignition, alarm }, speed, latitude, longitude } = deviceData;
 
-    const speedLimit = 60; 
+    const speedLimit = 60;
 
     if (!deviceStatus[deviceId]) {
-        deviceStatus[deviceId] = { ignition, speed, status };
+        deviceStatus[deviceId] = { ignition, speed, status, lastActive: Date.now() };
         return;
     }
 
     if (deviceStatus[deviceId].ignition !== ignition) {
         const alert = createAlert(deviceData, 'Ignition'); // Create alert for ignition change
-        sendAlert(io,alert); // Send the alert
+        sendAlert(io, alert); // Send the alert
         alertsArray.push(alert);
-
     }
 
     if (speed > speedLimit && deviceStatus[deviceId].speed !== speedLimit) {
         const alert = createAlert(deviceData, 'speedLimitExceeded'); // Create alert for speed limit exceeded
-        sendAlert(io,alert); // Send the alert
+        sendAlert(io, alert); // Send the alert
         alertsArray.push(alert);
-
     }
 
     if (deviceStatus[deviceId].status !== status) {
         const alert = createAlert(deviceData, status === 'online' ? 'statusOnline' : status === 'offline' ? 'statusOffline' : 'statusUnknown');
-        sendAlert(io,alert); 
+        sendAlert(io, alert);
         alertsArray.push(alert);
-
     }
 
     if (alarm && deviceStatus[deviceId].alarm !== alarm) {
         data = alarm; // Store alarm data
-        const alert = createAlert(deviceData, 'alarm'); // Create alert for alarm
-        sendAlert(io,alert); // Send the alert
+        const alarmAlert = createAlert(deviceData, 'alarm'); // Create alert for general alarm
+        sendAlert(io, alarmAlert); // Send the alert
+        alertsArray.push(alarmAlert);
+        if (alarm === 'geofenceEnter') {
+            const geofenceEnteredAlert = createAlert(deviceData, 'geofenceEntered'); // Create alert for geofence entered
+            sendAlert(io, geofenceEnteredAlert); // Send the alert
+            alertsArray.push(geofenceEnteredAlert);
+        } else if (alarm === 'geofenceExit') {
+            const geofenceExitedAlert = createAlert(deviceData, 'geofenceExited'); // Create alert for geofence exited
+            sendAlert(io, geofenceExitedAlert); // Send the alert
+            alertsArray.push(geofenceExitedAlert);
+        }
         data = null; // Reset data
-        alertsArray.push(alert);
-
     }
 
     // Check for device stopped status
-    // if (speed <= stopLimit && deviceStatus[deviceId].deviceSpeed !== stopLimit) {
-    //     const alert = createAlert(deviceData, 'deviceStopped'); // Create alert for device stopped
-    //     sendAlert(alert); // Send the alert
-    // } else if (speed > stopLimit && deviceStatus[deviceId].deviceSpeed !== stopLimit) {
-    //     const alert = createAlert(deviceData, 'deviceMoving'); // Create alert for device moving
-    //     sendAlert(alert); // Send the alert
-    // }
+    if (speed <= stopLimit && deviceStatus[deviceId].speed !== speed && deviceStatus[deviceId].lastAlertType !== 'deviceStopped') {
+        const alert = createAlert(deviceData, 'deviceStopped'); // Create alert for device stopped
+        sendAlert(io, alert); // Send the alert
+        alertsArray.push(alert);
+        deviceStatus[deviceId].lastAlertType = 'deviceStopped'; // Update last alert type
+    } else if (speed > stopLimit && deviceStatus[deviceId].speed !== speed && deviceStatus[deviceId].lastAlertType !== 'deviceMoving') {
+        const alert = createAlert(deviceData, 'deviceMoving'); // Create alert for device moving
+        sendAlert(io, alert); // Send the alert
+        alertsArray.push(alert);
+        deviceStatus[deviceId].lastAlertType = 'deviceMoving'; // Update last alert type
+    } else if (Date.now() - deviceStatus[deviceId].lastActive >= inactiveThreshold && deviceStatus[deviceId].lastAlertType !== 'deviceInactive') {
+        const alert = createAlert(deviceData, 'deviceInactive'); // Create alert for device inactive
+        sendAlert(io, alert); // Send the alert
+        alertsArray.push(alert);
+        deviceStatus[deviceId].lastAlertType = 'deviceInactive'; // Update last alert type
+    }
 
     deviceStatus[deviceId].ignition = ignition;
     deviceStatus[deviceId].speed = speed;
     deviceStatus[deviceId].status = status;
-    deviceStatus[deviceId].deviceSpeed = deviceSpeed;
+    deviceStatus[deviceId].deviceSpeed = speed;
     deviceStatus[deviceId].alarm = alarm;
-    deviceStatus[deviceId].stopLimit = stopLimit;
+    deviceStatus[deviceId].lastActive = Date.now(); // Update last active time
 };
+
 const createAlert = (deviceData, type) => {
     const { attributes: { ignition, speed, alarm }, status, latitude, longitude } = deviceData; // Destructuring device data
     const ignitionStatus = ignition ? 'ignitionOn' : 'ignitionOff'; // Determine ignition status
@@ -87,8 +102,14 @@ const createAlert = (deviceData, type) => {
         message = `Device ${deviceData.deviceId} is moving! Speed: ${speed} km/h`;
     } else if (type === 'deviceStopped') {
         message = `Device ${deviceData.deviceId} is stopped! Speed: ${speed} km/h`;
+    } else if (type === 'deviceInactive') {
+        message = `Device ${deviceData.deviceId} has been inactive for 24 hours!`;
     } else if (type === 'alarm') {
         message = `Alarm for ${deviceData.deviceId} is ${alarm}!`;
+    } else if (type === 'geofenceEntered') {
+        message = `Device ${deviceData.deviceId} has entered the geofence!`;
+    } else if (type === 'geofenceExited') {
+        message = `Device ${deviceData.deviceId} has exited the geofence!`;
     } else if (type === "statusOnline" ? "statusOnline" : type === "statusOffline" ? "statusOffline" : "statusUnknown") {
         message = `Status of ${deviceData.deviceId} is ${status === 'online' ? 'online' : status === 'offline' ? 'offline' : 'unknown'}`;
     }
@@ -104,10 +125,9 @@ const createAlert = (deviceData, type) => {
     };
 };
 
-
-const sendAlert = async (io,alert) => {
+const sendAlert = async (io, alert) => {
     // console.log('Alert sent:', alert); // Log the alert
-    const savedAlert = await new Alert(alert).save(); 
+    const savedAlert = await new Alert(alert).save();
 
     // await savedAlert.save(); 
 };
@@ -149,7 +169,7 @@ export const AlertFetching = async (io) => {
                 password: '123456@'
             }
         });
-        const deviceData = resdevice.data; 
+        const deviceData = resdevice.data;
 
         const deviceApiData = new Map(deviceData.map(item => [item.id, item]));
 
@@ -159,7 +179,7 @@ export const AlertFetching = async (io) => {
         filteredDevices.forEach(obj1 => {
             const match = deviceApiData.get(obj1.deviceId);
             if (match) {
-                obj1.status = match.status; 
+                obj1.status = match.status;
             }
         });
 
@@ -174,6 +194,6 @@ export const AlertFetching = async (io) => {
         console.log('All Device IDs:', Array.from(selectedDeviceIds));
 
     } catch (error) {
-        console.error('Error fetching data:', error); 
+        console.error('Error fetching data:', error);
     }
 }
