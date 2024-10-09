02890b6125b0e5f214c8f7d582b842e3f2d828ac
Gagan Raghuwanshi <raghu1cgagan@gmail.com>
2024-10-09 13:09:28 +0530

    made live data API

--- routes/deviceHistory.route.js
+++ routes/deviceHistory.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deviceAllHistory, deviceHistoryByTime, deviceStopage, deviceTripsWithRoute, showOnlyDeviceTripStartingPointAndEndingPoint } from "../controllers/DeviceHistory.controller.js";
+import { deviceAllHistory, deviceHistoryByTime, deviceStopage, deviceTripsWithRoute, liveData, showOnlyDeviceTripStartingPointAndEndingPoint } from "../controllers/DeviceHistory.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/device-history-by-time", deviceHistoryByTime);
 router.get("/device-trips-with-route", deviceTripsWithRoute);
 router.get("/show-only-device-trips-startingpoint-endingpoint", showOnlyDeviceTripStartingPointAndEndingPoint);
 router.get("/device-stopage", deviceStopage);
+router.post("/live-data", liveData);
 
 export default router;
