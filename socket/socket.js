5e029d1fa97ea2c8447f1e9f50d514d6df0fc00b
Gagan Raghuwanshi <143127419+Gagan-Raghuwanshi@users.noreply.github.com>
2024-10-09 11:51:42 +0530

    Update socket.js
    allowing all origin

--- socket/socket.js
+++ socket/socket.js
@@ -10,7 +10,7 @@ import jwt from "jsonwebtoken";
 export const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:3000"],
+      origin: "*",                             // ["http://localhost:5173", "http://localhost:3000"],
       methods: ["GET", "POST"],
       credentials: true,
     },
