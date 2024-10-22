ec4265b577cd963a3d9c13fce0423e443d07eaa2
Yash Dahiwale <yashdahiwale3@gmail.com>
2024-10-22 17:05:20 +0530

    commented socket code

--- controllers/superadminControlller.js
+++ controllers/superadminControlller.js
@@ -17,6 +17,27 @@ export const registersuperAdmin = async (req, res) => {
 };
 
 
+// update superadmin
+export const updatesuperAdmin = async (req, res) => {
+  const { id } = req.params;
+  const { email, password, username } = req.body;
+
+  // Create an update object with only the fields that are provided
+  const updateFields = {};
+  if (email) updateFields.email = email;
+  if (password) updateFields.password = await bcrypt.hash(password, 10); // Hash the password
+  if (username) updateFields.username = username;
+
+  // Update superadmin
+  const superadmin = await SuperAdmin.findByIdAndUpdate(
+    id,
+    updateFields,
+    { new: true }
+  );
+
+  return res.status(200).json({ message: 'Superadmin updated successfully', superadmin });
+};
+
 
 
 
--- routes/superadminRoute.js
+++ routes/superadminRoute.js
@@ -1,9 +1,10 @@
 // routes/authRoutes.js
 import express from 'express';
-import { registersuperAdmin} from '../controllers/superadminControlller.js';
+import { registersuperAdmin, updatesuperAdmin } from '../controllers/superadminControlller.js';
 const router = express.Router();
 
 router.post('/register', registersuperAdmin);
 
+router.put('/:id', updatesuperAdmin);
 
 export default router;
